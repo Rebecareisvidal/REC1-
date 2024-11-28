797717c05f901ff29a2e2569579edffe7e1f1d79
Rebeca Reis Vidal <106605949+Rebecareisvidal@users.noreply.github.com>
2024-11-28 20:10:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+    document.getElementById('motivacao-button').addEventListener('click', function() {
+       
+        document.getElementById('motivacao-message').textContent = 'Você é capaz de alcançar todos os seus sonhos!';
+    });
+});
